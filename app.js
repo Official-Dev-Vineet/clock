4e9aba12c7184441a9b14e7900572b8bac91453e
vineet <viratsinghkaharwar8923@gmail.com>
2023-05-16 18:19:14 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".seconds");
+const digitalClock = document.querySelector(".digital-clock");
+setInterval(() => {
+  const d = new Date(); //object of date()
+  const hr = d.getHours();
+  const min = d.getMinutes();
+  const sec = d.getSeconds();
+  const hr_rotation = 30 * hr + min / 2; //converting current time
+  const min_rotation = 6 * min;
+  const sec_rotation = 6 * sec;
+
+  hour.style.transform = `translate(-50%, -100%) rotate(${hr_rotation}deg`;
+  minute.style.transform = ` translate(-50%, -100%) rotate(${min_rotation}deg`;
+  second.style.transform = ` translate(-50%, -100%) rotate(${sec_rotation}deg`;
+  const now = new Date();
+  const dhr = now.getHours().toLocaleString("en-US")
+  const dmin = now.getMinutes();
+  const dsec = now.getSeconds();
+  digitalClock.innerHTML = `${dhr < 10 ? "0" + dhr : dhr}:${
+    dmin < 10 ? "0" + dmin : dmin
+  }:${dsec < 10 ? "0" + dsec : dsec}`;
+}, 1000);
